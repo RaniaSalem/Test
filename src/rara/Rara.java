c21040b39b734cf6c55394ceb4d7e645274a278d
toshiba <toshiba@fge>
2015-12-15 03:19:42 -0800

    tow

--- src/rara/Rara.java
+++ src/rara/Rara.java
@@ -11,9 +11,7 @@ package rara;
  */
 public class Rara {
 
-    /**
-     * @param args the command line arguments
-     */
+    
     public static void main(String[] args) {
         System.out.println("Rara");
     }
